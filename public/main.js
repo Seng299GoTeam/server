f71bda932d929dbe34c2c8fc65b33f1e7789c95e
averyhiebert <fishstudios11@gmail.com>
2016-07-09 22:07:22 -0700

    Improvements to ai move

--- public/main.js
+++ public/main.js
@@ -9,6 +9,9 @@ var ui = new UI();
 ui.show ( "startPage");
 var game;
 
+//TEMP:
+var ai = new aiInterface('roberts.seng.uvic.ca','/ai/random','30000');
+
 
 
 
@@ -19,44 +22,48 @@ function startNewGame(){
     game = new Game ( "hotseat", selected );
     ui.board(game.board.grid );
     ui.show ( "boardPage" );
-
 }
-
-
-
-    
 
 
 function boardClickHandler(x,y){
     var move = new go.Move (x, y, game.currentPlayer , false);
 
-    function successfulMove (){         // What should happen if a move is successfull
-        ui.board(game.board.grid);
-    }
-    function invalidMove ( message ){   // What should happen if a move is invalid
-        ui.invalid( message );
-    }
-
     game.attemptMove(move,successfulMove,invalidMove);
 }
 
+function successfulMove (){          // What should happen if a move is successfull
+    ui.board(game.board.grid);
+    getAiMoveTemp();
+}
+
+function invalidMove ( message ){    // What should happen if a move is invalid
+    ui.invalid( message );
+}
+
+function gameEnds (){               // What should happen if the move results to end the game
+    ui.end()
+}
+
 function pass(){
-    function successfulMove (){          // What should happen if a move is successfull
-        ui.board(game.board.grid);
-    }
-    function invalidMove ( message ){    // What should happen if a move is invalid
-        ui.invalid( message );
-    }
-    function gameEnds (){               // What should happen if the move results to end the game
-        ui.end()
-    }
-    
-    
-    
-    
     var move = new go.Move (-1, -1, game.currentPlayer , true);
     game.attemptMove(move,successfulMove,invalidMove, gameEnds);
-    
+}
+
+//TEMP: This is just for testing that we can deal with ai moves.
+function getAiMoveTemp(){
+    ai.getMove(10,aiMoveTemp,function(){});
+}
+
+//TEMP: This is just for testing that we can deal with ai moves.
+//Takes a move
+function aiMoveTemp(move){
+    game.attemptMove(move,successfulAiMove,invalidMove,gameEnds);
+}
+
+
+//TEMPORARY - still working out how AI will work.
+function successfulAiMove(){
+    ui.board(game.board.grid);
 }
 
 
