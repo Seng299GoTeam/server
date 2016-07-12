0dc69caa2008cd9193736a365f2ca78165f6b418
Michael Demone <m_d_emone@hotmail.com>
2016-07-12 15:27:23 -0700

    Added backend network interface

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express    = require("express");
 var http = require("http");
 var bodyparser = require("body-parser");
-var mongo = require("./mongoDB");
+var mongo = require("./MongoDB");
 
 var app = express();
 
@@ -14,8 +14,12 @@ app.use(bodyparser.json());
 app.use(express.static('public'));
 
 
+// Create database
+var db = new mongo(null, null, "testing"); 
 
-
+db.connect(function(err) {
+	
+});
 
 /**
  * Handle a request for task data.
@@ -27,22 +31,87 @@ app.get("/data", function (req, res) {
 
 app.get("/checkGame", function(req, res) {
 	console.log("GET Request to: /checkGame");
+	// Will return the current player of a game
+	// given the game id
 	
-	// Get the game id from the 
-	var gameid = req.body();
+	var gameid = req.body.gameData._id;
 	
-	mongo.getGamePlayer(gameid, function(player, err) {
+	db.getGamePlayer(gameid, function(player, err) {
 		if(!err)
 			res.send(player);
 		else {
-			console.log("ERROR: " + err);
 			res.send(err);
 		}
 	});
 });
 
-app.get("/getGame")
+app.get("/getGame", function(req, res) {
+	console.log("GET Request to: /getGame");
+	// Will return the game associated with a game id
+	// as a JSON object or will return an error message
+	
+	var gameid = req.body.gameData.id;
+	
+	
+	db.getGame(gameid, function(game, err) {
+		if(err)
+			res.send(JSON.stringify(game));
+		else {
+			res.send(err);
+		}
+	});
+	
+	
+});
 
+app.get("/createGame", function(req, res) {
+	console.log("GET Request to: /createGame");
+	// Will create a game given a game object and will return
+	// an id (or an error)
+	
+	var game = JSON.parse(req.body.gameData);
+	
+	db.addGame(game, function(id, err) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.send(id);
+		}
+	});
+	
+});
+
+
+app.post("/updateGame", function(req, res) {
+	console.log("POST Request to: /updateGame");
+	// will update the game given a game object and will return
+	// either "Success" or an error message
+	
+	var updatedGame = JSON.parse(req.body.gameData);
+	
+	db.updateGame(game, function(err) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.send("Success");
+		}
+	});
+});
+
+app.post("/endGame", function(req, res) {
+	console.log("POST Request to: /endGame");
+	// will end the game given an id and will return either 
+	// "Success" or an error message
+	
+	var id = req.body.gameData._id;
+	
+	deleteGame(id, function(err) {
+		if(err)
+			res.send(err);
+		else
+			res.send("Success");
+	});
+});
 
 app.post("/ai",function (req,res){
     console.log("POST Request to: /ai");
